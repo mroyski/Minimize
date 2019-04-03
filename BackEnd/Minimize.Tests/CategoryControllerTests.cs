2c002aeff10f1ee2871b14fe006ac878ef577da4
khanishvili <khnshvl@gmail.com>
2019-04-03 18:06:33 -0400

    GetAll Test

--- BackEnd/Minimize.Tests/CategoryControllerTests.cs
+++ BackEnd/Minimize.Tests/CategoryControllerTests.cs
@@ -1,11 +1,47 @@
-
 using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Minimize.Controllers;
+using Minimize.Models;
+using Minimize.Repositories;
+
 using Xunit;
+using NSubstitute;
+using System.Collections.Generic;
 
 namespace Minimize.Tests
 {
     public class CategoryControllerTests
     {
-       
+        CategoryController underTest;
+        ICategoryRepository repo;
+
+        public CategoryControllerTests()
+        {
+            repo = Substitute.For<ICategoryRepository>();
+            underTest = new CategoryController(repo);
+        }
+        [Fact]
+         public void  GetAll_Should_Return_All_Categories()
+        {
+            var expectedmodel = new List<Category>() { new Category() , new Category() , new Category() };
+            repo.GetAll().Returns(expectedmodel);
+
+            var result = underTest.GetAll();
+
+            Assert.Equal(3, result.Value.Count());
+        }
+
+        [Fact]
+        public void Get_Should_Return_Expected_Category_By_Id()
+        {
+            var expectedId = 1;
+            var expectedCategory = new Category() { CategoryId = expectedId , CategoryDescription="Test" ,CategoryImg="test IMG" , CategoryName="Testing" , Posts= new List<Post>()};
+            repo.GetById(expectedId).Returns(expectedCategory);
+
+            var model = underTest.Get(expectedId);
+
+            Assert.Equal(expectedCategory, model);
+        }
     }
 }
