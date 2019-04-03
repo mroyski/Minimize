2863e2a3da281eb3d6238da6b096aab4bfc819fd
khanishvili <khnshvl@gmail.com>
2019-04-03 17:32:55 -0400

    Initial Commit

--- BackEnd/Minimize.Tests/CategoryControllerTests.cs
+++ BackEnd/Minimize.Tests/CategoryControllerTests.cs
@@ -6,22 +6,6 @@ namespace Minimize.Tests
 {
     public class CategoryControllerTests
     {
-        //CategoryController underTest;
-        //ICategoryRepository repo;
-
-        //public CategoryControllerTests()
-        //{
-        //    repo = Substitute.For<ICategoryRepository>();
-        //    underTest = new CategoryController(repo);
-        //}
-
-
-        //[Fact]
-        //public void Get_Returns_A_List_Of_Categories()
-        //{
-        //    var result = underTest.GetAll();
-
-
-        //}
+       
     }
 }
--- BackEnd/Minimize/Controllers/CategoryController.cs
+++ BackEnd/Minimize/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Minimize.Models;
 using Minimize.Repositories;
@@ -19,7 +20,8 @@ namespace Minimize.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Category>> GetAll()
         {
-            return Ok(categoryRepo.GetAll()); 
+            var model = categoryRepo.GetAll().ToArray();
+            return model;
         }
 
         [HttpGet("{id}")]
