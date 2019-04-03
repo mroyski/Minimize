a84f858c79390f2ff49b74c157026c6f7d3913de
Mike <mroyski@gmail.com>
2019-04-03 10:26:59 -0400

    Imported FontAwesome

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import CategoryIndex from "./Components/CategoryIndex";
-import CatDetailsPage from "./CatDetailsPage";
+import CatDetailsPage from "./Components/CatDetailsPage";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTshirt } from "@fortawesome/free-solid-svg-icons";
+library.add(faTshirt);
 
 function Category() {
   return <CategoryIndex />;
@@ -15,6 +19,7 @@ function AppRouter() {
   return (
     <Router>
       <div>
+        Favorite Food: <FontAwesomeIcon icon="tshirt" />
         <Route path="/" exact component={Category} />
         <Route path="/category/index" component={CatDetails} />
       </div>
