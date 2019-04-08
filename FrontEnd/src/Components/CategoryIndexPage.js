bce6ea16800f4df455ccf1c2f48a5f09593a0cc5
Mike <mroyski@gmail.com>
2019-04-07 21:50:45 -0400

    Added Index CSS

--- FrontEnd/src/Components/CategoryIndexPage.js
+++ FrontEnd/src/Components/CategoryIndexPage.js
@@ -17,11 +17,12 @@ export default props => {
   const categoryLinks = props.categories.map(item => (
     <Link to={`/category/${item.categoryId}`}>
       <FontAwesomeIcon icon={item.categoryImg} />
-      <p>{item.categoryName}</p>
+      <p id="CatName">{item.categoryName}</p>
     </Link>
   ));
   return (
     <div className="CatIndexBody">
+      <h1 id="MiniTitle">minimize</h1>
       <ul id="CatIndexUl">
         <div id="CatLists">{categoryLinks}</div>
       </ul>
