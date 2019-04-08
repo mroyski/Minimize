0a60a5b8b8de60691a4b502253189f28e76d63aa
Mike <mroyski@gmail.com>
2019-04-08 19:53:52 -0400

    added switch button for graph type

--- FrontEnd/src/Components/Chart.js
+++ FrontEnd/src/Components/Chart.js
@@ -14,28 +14,59 @@ class Chart extends Component {
     displayLegend: true,
     legendPosition: "right"
   };
+
+  toggleGraphType = () => {
+    const doesShow = this.state.graphType;
+    this.setState({ graphType: !doesShow });
+  };
+
   render() {
-    const graphData = {};
-    return (
-      <div className="Chart">
-        <Bar
-          data={this.state.chartData}
-          width={1000}
-          height={250}
-          options={{
-            title: {
-              display: this.props.displayTitle,
-              text: "Items Removed By Category",
-              fontSize: 20
-            },
-            legend: {
-              display: this.props.displayLegend,
-              position: this.props.legendPosition
-            }
-          }}
-        />
-      </div>
-    );
+    let graphDisplay = null;
+    if (this.state.graphType) {
+      graphDisplay = (
+        <div className="Chart">
+          <button onClick={this.toggleGraphType}>Display</button>
+          <Bar
+            data={this.state.chartData}
+            width={1000}
+            height={250}
+            options={{
+              title: {
+                display: this.props.displayTitle,
+                text: "Items Removed By Category",
+                fontSize: 20
+              },
+              legend: {
+                display: this.props.displayLegend,
+                position: this.props.legendPosition
+              }
+            }}
+          />
+        </div>
+      );
+    } else
+      graphDisplay = (
+        <div className="Chart">
+          <button onClick={this.toggleGraphType}>Display</button>
+          <Pie
+            data={this.state.chartData}
+            width={1000}
+            height={250}
+            options={{
+              title: {
+                display: this.props.displayTitle,
+                text: "Items Removed By Category",
+                fontSize: 20
+              },
+              legend: {
+                display: this.props.displayLegend,
+                position: this.props.legendPosition
+              }
+            }}
+          />
+        </div>
+      );
+    return <div>{graphDisplay}</div>;
   }
 }
 
