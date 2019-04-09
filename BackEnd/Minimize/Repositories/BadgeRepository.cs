27f5e507a88b4bd6c0a7aeae2367e5bf9d358ada
Alden <aj.molina092@gmail.com>
2019-04-09 12:56:46 -0400

    Added badge model structure

--- BackEnd/Minimize/Repositories/BadgeRepository.cs
+++ BackEnd/Minimize/Repositories/BadgeRepository.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Minimize.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Minimize.Repositories
 {
-    public class BadgeRepository
+    public class BadgeRepository : IBadgeRepository
     {
+        MinimizeContext db;
+
+        public BadgeRepository(MinimizeContext db)
+        {
+            this.db = db;
+        }
+
+
+        public IEnumerable<Badge> GetAll()
+        {
+            return db.Badges.ToList();
+        }
     }
 }
