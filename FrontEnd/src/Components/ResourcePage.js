0d294745578d7f561bba2920cfcda2e53f8b627d
Bpjd21 <bpjd21@yahoo.com>
2019-04-08 23:33:44 -0400

    Map added but has issues

---
+++ FrontEnd/src/Components/ResourcePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import GoogleMaps, { MapContainer } from "./MapContainer";
+
+class ResourcePage extends Component {
+    render() {
+        return (
+            <div id="Map">                
+            </div>
+        );
+    }
+}
+
+export default ResourcePage;
