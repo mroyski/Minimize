aed49a1e9d2cdc6bac40e40db4420116bc31d79a
Alden <aj.molina092@gmail.com>
2019-04-09 13:06:42 -0400

    Created badge index page container with states

---
+++ FrontEnd/src/Components/BadgeIndexPageContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { GetAll } from "./Library/HelperFunctions";
+
+class BadgeIndexPageContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+        badges = []
+    }
+  }
+  render() {
+    return <div />;
+  }
+}
+
+export default BadgeIndexPageContainer;
