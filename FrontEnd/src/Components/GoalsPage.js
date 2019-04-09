15cd552ebc4bea2dccc47bcbacbb096e483b9fca
Mike <mroyski@gmail.com>
2019-04-09 14:07:19 -0400

    pulled selected date from calendar

--- FrontEnd/src/Components/GoalsPage.js
+++ FrontEnd/src/Components/GoalsPage.js
@@ -1,18 +1,10 @@
 import React, { Component } from "react";
-import Calendar from "react-calendar";
-
+import Calendar from "./Calendar";
 class GoalsPage extends Component {
-  state = {
-    date: new Date()
-  };
-  onChange = date => this.setState({ date });
   render() {
     return (
       <div>
-        <Calendar onChange={this.onChange} value={this.state.date} />
-        <div id="goalContainer">
-          <p>Date: {this.state.date.day}</p>
-        </div>
+        <Calendar />
       </div>
     );
   }
