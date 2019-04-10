f6d99f552dc6b043ed0541cdff4a0df4d0138ea3
Mo <mshwss@gmail.com>
2019-04-10 11:17:10 -0400

    donate links

--- FrontEnd/src/Components/ResourcePage.js
+++ FrontEnd/src/Components/ResourcePage.js
@@ -19,18 +19,18 @@ class ResourcePage extends Component {
         <div className="Simplify">
           <a href="https://simplifyandpursue.com">simplifyandpursue.com</a>
         </div>
-        <div className="ReasourceLinks">
+        <div className="DonateLinks">
       <a href="https://www.goodwill.org/donate-and-shop/donate-stuff">Goodwill</a>
       <a href="https://satruck.org/">Salvation Army</a>
       <a href="https://vva.org/donate/">Vietnam Veterans of America</a>
       <a href="https://roomtogrow.org/index.php/get-involved#donate-baby-items">Room to Grow</a>
       <a href="https://soles4souls.org/get-involved/give-shoes/">Soles 4 Souls</a>
       <a href="http://donationtown.org/donation-pick-up.html">Donation Town</a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
+      <a href="https://www.thearc.org/get-involved/ways-to-give/donate-physical-items">The ARC</a>
+      <a href="https://www.savers.com/donate">Savers</a>
+      <a href="http://indigorescue.org/?page_id=18">Indigo rescue</a>
+      <a href="https://new-eyes.org/join-the-vision">New Eyes</a>
+      <a href="https://www.burlington.com/warm-coats-warm-hearts-drive.aspx?utm=marketing&_sp=1700a376-0750-4ee3-99d9-43b19620af17.1554691319024">Burlington</a>
 
       </div>
       </div>
