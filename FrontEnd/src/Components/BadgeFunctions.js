87d3c4f678deb54fd509edbf71c85f1672cca890
Alden <aj.molina092@gmail.com>
2019-04-11 11:07:36 -0400

    Have total items for each category

--- FrontEnd/src/Components/BadgeFunctions.js
+++ FrontEnd/src/Components/BadgeFunctions.js
@@ -16,8 +16,30 @@ class BadgeFunctions extends Component {
   render() {
     // Get Total Items Removed
     let itemsCount = 0;
-    this.state.posts.map(item => (itemsCount += item.removedItems));
-    console.log(itemsCount);
+    let clothingCount = 0;
+    let furnitureCount = 0;
+    let toolsCount = 0;
+    let electronicsCount = 0;
+    let toysCount = 0;
+    let miscCount = 0;
+
+    this.state.posts.forEach(item => {
+      itemsCount += item.removedItems;
+
+      if (item.categoryId == 1) {
+        clothingCount += item.removedItems;
+      } else if (item.categoryId == 2) {
+        furnitureCount += item.removedItems;
+      } else if (item.categoryId == 3) {
+        toolsCount += item.removedItems;
+      } else if (item.categoryId == 4) {
+        electronicsCount += item.removedItems;
+      } else if (item.categoryId == 5) {
+        toysCount += item.toysCount;
+      }
+    });
+
+    console.log(toolsCount);
 
     return <div />;
   }
