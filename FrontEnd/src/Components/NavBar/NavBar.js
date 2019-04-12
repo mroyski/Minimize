654765b891b00ff9034146aa587aa63d5b2089d4
khanishvili <khnshvl@gmail.com>
2019-04-12 11:39:53 -0400

    Fixed Nav bar

--- FrontEnd/src/Components/NavBar/NavBar.js
+++ FrontEnd/src/Components/NavBar/NavBar.js
@@ -1,38 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import HamburgerToggle from "../Hamburger/HamburgerToggle";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import HamburgerToggle from '../Hamburger/HamburgerToggle';
 import './NavBar.css';
 
 const navbar = props => (
-    <header className="navbar">
-        <nav className="navbar_navigation"> 
-        <div className="hamburger">
-            <HamburgerToggle click={props.hamburgerClickHandler}/>
-        </div>   
+  <header className="navbar">
+    <nav className="navbar_navigation">
+      <div className="hamburger">
+        <HamburgerToggle click={props.hamburgerClickHandler} />
+      </div>
       <div className="logo">
         <Link to={`/`}>
           <img src="/Images/Logo3.png" alt="logo" />
         </Link>
       </div>
-      <div className="spacer"/>
+      <div className="spacer" />
       <div className="navbar_items">
-      <ul>
-        <li><Link to={`/`} className="link">
-          GOALS
-        </Link></li>
-        <li><Link to={`/`} className="link">
-          BADGES
-        </Link></li>
-        <li><Link to={"/profile"} className="link">
-          PROFILE
-        </Link></li>
-        <li><Link to={"/ResourcePage"} className="link">
-          RESOURCES
-        </Link></li>
+        <ul>
+          <li>
+            <Link to={`/goals`} className="link">
+              GOALS
+            </Link>
+          </li>
+          <li>
+            <Link to={`/badges`} className="link">
+              BADGES
+            </Link>
+          </li>
+          <li>
+            <Link to={'/profile'} className="link">
+              PROFILE
+            </Link>
+          </li>
+          <li>
+            <Link to={'/ResourcePage'} className="link">
+              RESOURCES
+            </Link>
+          </li>
         </ul>
       </div>
-      </nav>
-    </header>
+    </nav>
+  </header>
 );
 
 export default navbar;
