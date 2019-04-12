771077e0a2402e2f87b473a49a283cffa84f16e6
Brian <bpjd21@yahoo.com>
2019-04-12 09:56:49 -0400

    fixed some sizing

--- FrontEnd/src/Components/Hamburger/SideNav.js
+++ FrontEnd/src/Components/Hamburger/SideNav.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./SideNav.css";
 
 const sideNav = props => (
-    <nav className="side_nav">
-      <ul>
-        <li><Link to={`/`} className="link">
+  <nav className="side_nav">
+    <ul>
+      <li />
+      <li />
+      <li>
+        <Link to={`/`} className="link">
           GOALS
-        </Link></li>
-        <li><Link to={`/`} className="link">
+        </Link>
+      </li>
+      <li>
+        <Link to={`/`} className="link">
           BADGES
-        </Link></li>
-        <li><Link to={"/profile"} className="link">
+        </Link>
+      </li>
+      <li>
+        <Link to={"/profile"} className="link">
           PROFILE
-        </Link></li>
-        <li><Link to={"/ResourcePage"} className="link">
+        </Link>
+      </li>
+      <li>
+        <Link to={"/ResourcePage"} className="link">
           RESOURCES
-        </Link></li>
-        </ul>  
-    </nav>
-
-
+        </Link>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default sideNav;
