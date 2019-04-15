927381c92062517cd32964172fcb36cef51f7152
Alden <aj.molina092@gmail.com>
2019-04-15 08:13:09 -0400

    Finished category index styling

--- FrontEnd/src/Components/Post.js
+++ FrontEnd/src/Components/Post.js
@@ -18,13 +18,17 @@ class Post extends Component {
     return (
       <li className="post-list">
         <div id="postDetails">
-          {/* <h4>Post Number: {this.props.post.postId}</h4> */}
-          <p>Time: {postTime}</p>
-          <p>Total Items: {totalItems}</p>
-          <p>Removed Items: {removedItems}</p>
-          <p>Description: {postDescription}</p>
-          <div className="post-img">
-            <img src={postImgPath} alt="UserPhoto" />
+          <div className="post">
+            <div className="post-info">
+              {/* <h4>Post Number: {this.props.post.postId}</h4> */}
+              <p>Time: {postTime}</p>
+              <p>Total Items: {totalItems}</p>
+              <p>Removed Items: {removedItems}</p>
+              <p>Description: {postDescription}</p>
+            </div>
+            <div className="post-img">
+              <img src={postImgPath} alt="User Photo" />
+            </div>
           </div>
           <div className="post-button">
             <button onClick={() => this.removePost(postId)}>Delete</button>
