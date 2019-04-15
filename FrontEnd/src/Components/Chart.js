1a0ca7681f9ad2251649ef9425e94b43c8221404
Mike <mroyski@gmail.com>
2019-04-15 11:33:01 -0400

    made charts responsive, styled badges more

--- FrontEnd/src/Components/Chart.js
+++ FrontEnd/src/Components/Chart.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Bar, Pie } from "react-chartjs-2";
+import "./Chart.css";
 
 class Chart extends Component {
   constructor(props) {
@@ -28,8 +29,8 @@ class Chart extends Component {
           <button onClick={this.toggleGraphType}>Display</button>
           <Bar
             data={this.props.chartData}
-            width={1000}
-            height={250}
+            width={100}
+            height={50}
             options={{
               title: {
                 display: this.props.displayTitle,
@@ -50,8 +51,8 @@ class Chart extends Component {
           <button onClick={this.toggleGraphType}>Display</button>
           <Pie
             data={this.props.chartData}
-            width={1000}
-            height={250}
+            width={100}
+            height={50}
             options={{
               title: {
                 display: this.props.displayTitle,
@@ -66,7 +67,7 @@ class Chart extends Component {
           />
         </div>
       );
-    return <div>{graphDisplay}</div>;
+    return <div id="graphDisplay">{graphDisplay}</div>;
   }
 }
 
