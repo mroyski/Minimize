9b72908927df0b0654dc7c60852872e2a44c547b
khanishvili <khnshvl@gmail.com>
2019-04-15 11:44:45 -0400

    Goal model Change

---
+++ BackEnd/Minimize/Controllers/TrackerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Minimize.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackerController : ControllerBase
+    {
+        // GET: api/Tracker
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Tracker/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Tracker
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Tracker/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
