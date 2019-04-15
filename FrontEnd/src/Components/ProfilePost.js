d43958b440967525ddddd4ab3cd26c555aeef51b
Alden <aj.molina092@gmail.com>
2019-04-15 19:39:06 -0400

    Created profile post component

--- FrontEnd/src/Components/ProfilePost.js
+++ FrontEnd/src/Components/ProfilePost.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 
-class ProfilePost extends Component {
-  render() {
-    return <div />;
-  }
-}
-
-export default ProfilePost;
+export default props => {
+  const { posts } = this.props;
+  const profilePosts = posts.map(post => (
+    <div id="postDetails">
+      {/* <h4>Post Number: {this.props.post.postId}</h4> */}
+      <p>Time: {postTime}</p>
+      <p>Total Items: {totalItems}</p>
+      <p>Removed Items: {removedItems}</p>
+      <p>Description: {postDescription}</p>
+      <img src={postImgPath} alt="UserPhoto" />
+      <button onClick={() => this.removePost(postId)}>Delete</button>
+    </div>
+  ));
+  return <div>{profilePosts}</div>;
+};
