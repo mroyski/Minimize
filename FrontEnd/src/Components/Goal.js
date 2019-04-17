5e264e582339e1187f264e3bb3855f48a21f49de
Bpjd21 <bpjd21@yahoo.com>
2019-04-16 23:40:43 -0400

    added background color to goals

--- FrontEnd/src/Components/Goal.js
+++ FrontEnd/src/Components/Goal.js
@@ -5,12 +5,15 @@ export class Goal extends Component {
   render() {
     const { date, text, numberOfItems, category } = this.props;
     return (
-      <div className="newGoalContainer">
+      <div>
+       
+      <div className="newGoalContainer">          
         <div className="newGoals">
         <p>Date : {date}</p>
         <p>Text : {text}</p>
         <p> CategoryName : {category.categoryName}</p>
         <p> Total items to be removed : {numberOfItems}</p>
+      </div>
       </div>
       </div>
     );
