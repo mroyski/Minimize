c64222c8317fd641708ad68c2f638054d24aa77e
Alden <aj.molina092@gmail.com>
2019-04-18 09:25:56 -0400

    changed goal post info placeholder

--- FrontEnd/src/Components/Goal.js
+++ FrontEnd/src/Components/Goal.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./Goal.css";
 
 export class Goal extends Component {
@@ -6,15 +6,14 @@ export class Goal extends Component {
     const { date, text, numberOfItems, category } = this.props;
     return (
       <div>
-       
-      <div className="newGoalContainer">          
-        <div className="newGoals">
-        <p>Date : {date}</p>
-        <p>Text : {text}</p>
-        <p> CategoryName : {category.categoryName}</p>
-        <p> Total items to be removed : {numberOfItems}</p>
-      </div>
-      </div>
+        <div className="newGoalContainer">
+          <div className="newGoals">
+            <p>Date : {date}</p>
+            <p>Description : {text}</p>
+            <p>Category : {category.categoryName}</p>
+            <p>Total items to be removed : {numberOfItems}</p>
+          </div>
+        </div>
       </div>
     );
   }
